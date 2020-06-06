2bfd5cf530914e261a34b0331a227cbcc7e464dd
ya-shine <shunsoku.no.10@gmail.com>
2020-06-06 19:55:06 +0900

    add items routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'items#index'
-  resource :item
+  resources :items, only: [:new, :create]
   resource :order
   resource :shipping_addresses
   resources :mypage, only: :index
