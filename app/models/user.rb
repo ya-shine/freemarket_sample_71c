cad34015996674bd3276e8f90732608e9b49cd0b
ya-shine <shunsoku.no.10@gmail.com>
2020-06-08 00:35:07 +0900

    sign-in page validation settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true
+         validates :last_name, presence: true
+         validates :last_name_kana, presence: true
+         validates :birth_day, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, length: { minimum: 7 }
+         validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
