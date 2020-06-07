0f69bddaa0157a3ac05d2acc7b32a5d00530b1d8
Atsushiokabe <atsushiokabe@icloud.com>
2020-06-08 02:46:13 +0900

    created categories controller & model
    associated it to items model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_07_103903) do
+ActiveRecord::Schema.define(version: 2020_06_07_174120) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "ancestry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
