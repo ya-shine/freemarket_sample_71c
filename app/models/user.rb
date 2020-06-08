fcf600aa09a759fdf8182bc139c8c67c9f31bf1a
ya-shine <shunsoku.no.10@gmail.com>
2020-06-08 17:52:37 +0900

    add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
          validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :last_name_kana, presence: true
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
          validates :birth_day, presence: true
          validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }, length: { minimum: 7 }
          validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
