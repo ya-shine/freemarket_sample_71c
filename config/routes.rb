06a3caee49d6dc7120ef94cbc7333612b14f982b
ya-shine <shunsoku.no.10@gmail.com>
2020-06-09 00:24:57 +0900

    edit routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   devise_scope :user do
     get 'address', to: 'users/registrations#new_address'
