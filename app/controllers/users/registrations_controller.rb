a5051ab20a7abe15769d2c6f27b0da43aac1fda2
ya-shine <shunsoku.no.10@gmail.com>
2020-06-08 23:43:14 +0900

    edit users registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -5,14 +5,35 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    @user = User.new(sign_up_params)
+    unless @user.valid?
+      flash.now[:alert] = @user.errors.full_messages
+      render :new and return
+    end
+    session["devise.regist_data"] [user] = user: @user.attributes
+    session["devise.regist_data"][:user]["password"] = params[:user][:password]
+    @address = @user.build_address
+    render :new_address
+  end
+
+  def create_address
+    @user = User.new(session["devise.regist_data"]["user"])
+    @address = Address.new(address_params)
+    unless @address.valid?
+      flash.now[:alert] = @address.errors.full_messages
+      render :new_address and return
+    end
+    @user.build_address(@address.attributes)
+    session["address"] = @address.attributes
+    @user.save
+    sign_in(:user, @user)
+  end
 
   # GET /resource/edit
   # def edit
@@ -38,7 +59,11 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+
+  def address_params
+    params.require(:address).permit(:address, :zipcode, :prefecture, :city, :building, :phone_number)
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
