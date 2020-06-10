d00ecbb7c15814dfa46805a0ac13d6a9caace36d
Atsushiokabe <atsushiokabe@icloud.com>
2020-06-11 00:39:47 +0900

    modified fee & profit part

--- app/assets/javascripts/item_description.js
+++ app/assets/javascripts/item_description.js
@@ -1,6 +1,6 @@
 $(function(){
   var text_max = 1000;
-  $('#item_content').on('keyup',function(){
+  $('#item_content').on('input',function(){
     $('.count').text($(this).val().length + "/1000");
   })
 
@@ -8,9 +8,16 @@ $(function(){
     var totalPrice = $('#item_item').val();
     var profit = Math.round(totalPrice * 0.9)
     var fee = (totalPrice - profit)
-    $('.price-container__contents__fee--value').text(fee)
-    $('.price-container__contents__fee--value').prepend("￥")
-    $('.price-container__contents__profit--value').text(profit)
-    $('.price-container__contents__profit--value').prepend("￥")
+    var profitShow = profit.toLocaleString()
+    var feeShow = fee.toLocaleString()
+  if(totalPrice >= 300 && totalPrice <= 9999999){
+  $('.price-container__contents__fee--value').text(feeShow)
+  $('.price-container__contents__fee--value').prepend("￥")
+  $('.price-container__contents__profit--value').text(profitShow)
+  $('.price-container__contents__profit--value').prepend("￥")
+  } else{
+    $('.price-container__contents__fee--value').text('ー');
+    $('.price-container__contents__profit--value').text('ー');
+  }
   })
 })
