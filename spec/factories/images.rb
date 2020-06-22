83b749b241db27285d45a0f0a4278d40a6e269dd
Atsushiokabe <atsushiokabe@icloud.com>
2020-06-22 15:29:30 +0900

    added rspec test of image

--- spec/factories/images.rb
+++ spec/factories/images.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :image do
     id     {'1'}
     # image  {"'app/assets/images/test.jpg'"}
-    image  {Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg'))}
+    image  {Rack::Test::UploadedFile.new(Rails.root.join('spec/fixtures/test.jpg'))}
     created_at  {'2020-01-01'}
     updated_at  {'2020-01-01'}
   end
--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -14,13 +14,23 @@ describe Image do
       condition = create(:condition)
       brand = create(:brand)
       item = build(:item,condition_id: condition.id,delivery_fee_id: delivery_fee.id,shipping_day_id:shipping_day.id,shipping_method_id:shipping_method.id,user_id: user.id,ship_from_area_id: ship_from_area.id, category_id:category.id, size_id: size.id,brand_id:brand.id)
-      # image = build(:image,item_id:item.id)
-      image = Image.new(
-        image: "huuuu",
-        # image: Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.jpg')),
-        item_id: item.id
-      )
+      image = build(:image,item_id:item.id)
       expect(image).to be_valid
+    end
+
+    it 'imageが存在しなければ登録できないこと' do
+      user = create(:user)
+      category = create(:category)
+      size = create(:size)
+      ship_from_area = create(:ship_from_area)
+      delivery_fee = create(:delivery_fee)
+      shipping_day = create(:shipping_day)
+      shipping_method = create(:shipping_method)
+      condition = create(:condition)
+      brand = create(:brand)
+      item = build(:item,condition_id: condition.id,delivery_fee_id: delivery_fee.id,shipping_day_id:shipping_day.id,shipping_method_id:shipping_method.id,user_id: user.id,ship_from_area_id: ship_from_area.id, category_id:category.id, size_id: size.id,brand_id:brand.id)
+      image = build(:image, image: nil)
+      expect(image.errors[:image]).to include()
     end
   end
 end
