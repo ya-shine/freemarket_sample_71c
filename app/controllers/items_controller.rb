24af919b342be6e2c8a9951b799391621e1cae21
Atsushiokabe <atsushiokabe@icloud.com>
2020-06-27 15:49:48 +0900

    compiled codes in new method by using pluck

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,10 +9,7 @@ class ItemsController < ApplicationController
     @item = Item.new
     @item.images.new
     @parents = Category.all.order("id ASC")
-    @category_parent_array = []
-    Category.where(ancestry: nil).each do |parent|
-      @category_parent_array << parent.name
-    end
+    @category_parent_array = Category.where(ancestry: nil).pluck(:name)
   end
 
   def get_category_children
