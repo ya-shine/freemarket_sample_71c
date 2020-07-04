b340002e7e3bc95e846b291c9d696eeae7f2fdfe
Atsushiokabe <atsushiokabe@icloud.com>
2020-07-04 13:33:33 +0900

    set method as before_action on credit_cards_controller

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,5 @@
 class CreditCardsController < ApplicationController
+  before_action :brand_category_header, only: [:index,:new]
 
   def index
   end
@@ -6,4 +7,10 @@ class CreditCardsController < ApplicationController
   def new
   end
 
+  private
+
+  def brand_category_header
+    @category_parents = Category.where(ancestry: nil)
+    @brands = Brand.all
+  end
 end
