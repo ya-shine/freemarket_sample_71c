cd3ebd77d169a303cb8a11bd79220c5885f0f007
ya-shine <shunsoku.no.10@gmail.com>
2020-07-13 16:19:08 +0900

    edit jquery so that the calculated value is set when transitioning to the product edit page

--- app/assets/javascripts/item_new.js
+++ app/assets/javascripts/item_new.js
@@ -30,28 +30,25 @@ $(function(){
   })
 
   if (window.location.href.match(/\/items\/\d+\/edit/)) {
-    var text_max = 1000;
-    $('#item_description').load('input',function(){
-      $('.count').text($(this).val().length + "/1000");
-    })
+    $(function load() {
+      var text_max = 1000;
+      $('.count').text($('#item_description').val().length + "/1000");
 
-    $('#item_price').load('input',function(){
       var totalPrice = $('#item_price').val();
       var profit = Math.round(totalPrice * 0.9)
       var fee = (totalPrice - profit)
       var profitShow = profit.toLocaleString()
       var feeShow = fee.toLocaleString()
     if(totalPrice >= 300 && totalPrice <= 9999999){
-    $('.price-container__contents__fee--value').text(feeShow)
-    $('.price-container__contents__fee--value').prepend("￥")
-    $('.price-container__contents__profit--value').text(profitShow)
-    $('.price-container__contents__profit--value').prepend("￥")
+      $('.price-container__contents__fee--value').text(feeShow)
+      $('.price-container__contents__fee--value').prepend("￥")
+      $('.price-container__contents__profit--value').text(profitShow)
+      $('.price-container__contents__profit--value').prepend("￥")
     } else {
       $('.price-container__contents__fee--value').text('ー');
       $('.price-container__contents__profit--value').text('ー');
     }
-    })
-    $('#item_delivery_fee_id').load('change',function(){
+  
       var box_status = $('#item_delivery_fee_id').val();
       if(box_status == 1 || box_status == 2){
         $('.delivery-container__top-derivation').css('display','block');
