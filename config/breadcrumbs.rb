3338e64df73019f2f0b870e83ad76c13c2212986
Atsushiokabe <atsushiokabe@icloud.com>
2020-07-16 01:33:09 +0900

    set bread-crumbs

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -7,9 +7,52 @@ crumb :mypage do
   parent :root
 end
 
-# crumb :projects do
-#   link "Projects", projects_path
-# end
+crumb :payment_method do
+  link "支払い方法", credit_cards_path
+  parent :mypage
+end
+
+crumb :card_register do
+  link "クレジットカード情報入力", new_credit_card_path
+  parent :payment_method
+end
+
+crumb :registered_card do
+  link "登録クレジットカード情報"
+  parent :mypage
+end
+
+crumb :shipping_address do
+  link "発送元・お届け先住所変更", edit_shipping_address_path
+  parent :mypage
+end
+
+crumb :item_all do
+  link "商品一覧", items_path
+  parent :root
+end
+
+crumb :brands do
+  link "ブランド一覧", brands_path
+  parent :root
+end
+
+crumb :brand do |brand|
+  brand = Brand.find(params[:id])
+  link brand.name
+  parent :brands
+end
+
+crumb :categories do
+  link "カテゴリー一覧", categories_path
+  parent :root
+end
+
+crumb :category_parent do |category_parent|
+  category_parent = Category.find(params[:id])
+  link category_parent.name
+  parent :categories
+end
 
 # crumb :project do |project|
 #   link project.name, project_path(project)
