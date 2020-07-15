eaf6bd6a269d38c207674ffa016a44878fe53b42
ya-shine <shunsoku.no.10@gmail.com>
2020-07-15 23:25:12 +0900

    item model edit

--- app/models/item.rb
+++ app/models/item.rb
@@ -27,5 +27,9 @@ class Item < ApplicationRecord
 
     accepts_nested_attributes_for :images, allow_destroy:true
 
+    def like_user(user_id)
+      likes.find_by(user_id: user_id)
+    end
+
 
 end
