2de086dfacdc2462e72fa658bc6f5ea4c1e5792d
ya-shine <shunsoku.no.10@gmail.com>
2020-07-17 03:49:56 +0900

    update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
-  def new
+
+  def index
+    @users = User.all
   end
+
+  def show
+    @user = User.find(params[:id])
+    @brands = Brand.all
+    @category_parents = Category.all.where(ancestry: nil)
+  end
+
+  def likes
+    @user = current_user
+    @brands = Brand.all
+    @category_parents = Category.all.where(ancestry: nil)
+  end
+
 end
